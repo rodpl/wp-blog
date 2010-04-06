55502f24a67943c029f73ab9936a45288c47ce48
Daniel Dabrowski <dabrowski.daniel@gmail.com>
2010-04-06 11:23:15 +0200

    Update script can be run for single files.

--- update_wp.sh
+++ update_wp.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-scp -r wp/. rod@rod.42n.pl:~/public_html/other/42n_rod
+LOC="."
+REMOT=""
+if [ $1 ]; then
+    LOC=$1
+    REMOT=$1
+#    echo $1
+fi
+scp -r wp/$LOC rod@rod.42n.pl:~/public_html/other/42n_rod/$REMOT
